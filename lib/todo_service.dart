1e8b8020b4b2f53fe2af3deca5aeca6a14603127
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2023-11-11 02:36:56 +0800

    adding add Item and  getAllTodos Function

---
+++ lib/todo_service.dart
@@ -0,0 +1,19 @@
+import 'package:hive/hive.dart';
+import "todo_item.dart";
+
+class TodoService {
+  final String _boxName = "todoBox";
+
+  Future<Box<TodoItem>> get _box async =>
+      await Hive.openBox<TodoItem>(_boxName);
+
+  Future<void> addItem(TodoItem todoItem) async {
+    var box = await _box;
+    await box.add(todoItem);
+  }
+
+  Future<List<TodoItem>> getAllToDos() async {
+    var box = await _box;
+    return box.values.toList();
+  }
+}
